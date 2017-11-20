bc0694404d040d93e3f3377cddea1027709bddc7
JossCs <josscx@gmail.com>
2017-11-19 20:21:32 -0500

    segundo commit

--- Futbolista.py
+++ Futbolista.py
@@ -2,22 +2,43 @@
 
 class Futbolista:
 
-    def __init__(self, nombre, apellidos, edad, demarcacion, internacional):
+    def __init__(self,nombre, apellidos, edad, demarcacion, equipo):
         self.nombre = nombre
         self.apellidos = apellidos
         self.edad = edad
         self.demarcacion = demarcacion
-        self.internacional = internacional
+        self.equipo=equipo
 
     def toDBCollection (self):
-        return {
+        jugador = {
             "nombre":self.nombre,
             "apellidos":self.apellidos,
             "edad": self.edad,
             "demarcacion":self.demarcacion,
-            "internacional":self.internacional
+            "internacional":self.equipo
         }
+        return jugador
+
+    def __str__(self):
+        return "Nombre: %s - Apellidos: %s - Edad: %i - Demarcación: %s - Internacional: %r" \
+               %(self.nombre, self.apellidos, self.edad, self.demarcacion, self.equipo)
+
+
+
+class equipo:
+
+    def __init__(self,_id,nombre):
+
+        self._id=_id
+        self.nombre = nombre
+    def toDBCollection (self):
+        Equipo = {
+            "_id":self._id,
+            "nombre":self.nombre,
+        }
+        return Equipo
 
     def __str__(self):
         return "Nombre: %s - Apellidos: %s - Edad: %i - Demarcación: %s - Internacional: %r" \
                %(self.nombre, self.apellidos, self.edad, self.demarcacion, self.internacional)
+
--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 # -*- coding: utf-8 -*-
 from pymongo import MongoClient
 from Futbolista import Futbolista
+from Futbolista import equipo
 
 # Creo una lista de objetos futbolista a insertar en la BD
 futbolistas = [
-    Futbolista('Iker','Casillas',33,['Portero'],True),
-    Futbolista('Carles','Puyol',36,['Central', 'Lateral'],True),
-    Futbolista('Sergio','Ramos',28,['Lateral','Central'],True),
-    Futbolista('Andrés','Iniesta',30,['Centrocampista','Delantero'],True),
-    Futbolista('Fernando','Torres',30,['Delantero'],True),
-    Futbolista('Leo','Baptistao',22,['Delantero'],False)
+    Futbolista('Iker','Casillas',33,'Portero',1),
+    Futbolista('Carles','Puyol',36,'Central',2),
+    Futbolista('Sergio','Ramos',28,'Lateral',1),
+    Futbolista('Andrés','Iniesta',30,'Centrocampista',1),
+    Futbolista('Fernando','Torres',30,'Delantero',2),
+    Futbolista('Leo','Baptistao',22,'Delantero',2)
     ]
 
+river= {'juan':'perez','jose':'aquino'}
+barcel= {'pedro':'rodrigz','carlos':'kaka'}
+Equipos =[
+    equipo(1,'river'),
+    equipo(2,'barcelona')
 
+    ]
 # PASO 1: Conexión al Server de MongoDB Pasandole el host y el puerto
 mongoClient = MongoClient('localhost',27017)
 
@@ -23,8 +30,9 @@ db = mongoClient.Futbol
 
 # PASO 3: Obtenemos una coleccion para trabajar con ella
 collection = db.Futbolistas
-
+collection.drop()
 equipos = db.Equipos
+equipos.drop()
 
 
 # PASO 4: CRUD (Create-Read-Update-Delete)
@@ -33,17 +41,45 @@ equipos = db.Equipos
 for futbolista in futbolistas:
     collection.insert(futbolista.toDBCollection())
 
+for equ in Equipos:
+    equipos.insert(equ.toDBCollection())
+
 
 # PASO 4.2.1: "READ" -> Leemos todos los documentos de la base de datos
 cursor = collection.find()
+print cursor
 for fut in cursor:
     print "%s - %s - %i - %s - %r" \
-          %(fut['nombre'], fut['apellidos'], fut['edad'], fut['demarcacion'], fut['internacional'])
+         %(fut['nombre'], fut['apellidos'], fut['edad'], fut['demarcacion'], fut['internacional'])
 
 
+
+
+mienbr=[{'$lookup':{
+    'from':'Futbolistas',
+    'localField': '_id',
+    'foreignField' : 'internacional',
+    'as': 'miembro'}},
+        {'$match':
+         {'nombre':'river'}}]
+
+doc=equipos.aggregate(mienbr)
+print " jugadores de river"
+
+for f in doc:
+    for fut in f["miembro"]:
+         print "%s - %s - %i - %s " \
+         %(fut['nombre'], fut['apellidos'], fut['edad'], fut['demarcacion'])
+
+
+         
+    
+
+    
 
 
 collection.drop()
 equipos.drop()
 # PASO FINAL: Cerrar la conexion
 mongoClient.close()
+
